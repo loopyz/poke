6961c0ee828b9f8e591f827684699c3c8112c810
Lucy Guo <lucyguo@fb.com>
2014-06-19 18:43:44 -0700

    more stuff

---
+++ Poke/Poke/HomeViewController.h
@@ -0,0 +1,15 @@
+//
+//  HomeViewController.h
+//  Poke
+//
+//  Created by Lucy Guo on 6/19/14.
+//  Copyright (c) 2014 Poke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeViewController : UITabBarController
+
+@property (strong, nonatomic) UIColor *bgColor;
+
+@end
---
+++ Poke/Poke/HomeViewController.m
@@ -0,0 +1,73 @@
+//
+//  HomeViewController.m
+//  Poke
+//
+//  Created by Lucy Guo on 6/19/14.
+//  Copyright (c) 2014 Poke. All rights reserved.
+//
+
+#import "HomeViewController.h"
+
+#define SCREEN_WIDTH ((([UIApplication sharedApplication].statusBarOrientation == UIInterfaceOrientationPortrait) || ([UIApplication sharedApplication].statusBarOrientation == UIInterfaceOrientationPortraitUpsideDown)) ? [[UIScreen mainScreen] bounds].size.width : [[UIScreen mainScreen] bounds].size.height)
+#define SCREEN_HEIGHT ((([UIApplication sharedApplication].statusBarOrientation == UIInterfaceOrientationPortrait) || ([UIApplication sharedApplication].statusBarOrientation == UIInterfaceOrientationPortraitUpsideDown)) ? [[UIScreen mainScreen] bounds].size.height : [[UIScreen mainScreen] bounds].size.width)
+
+
+@interface HomeViewController ()
+
+@end
+
+@implementation HomeViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+      self.bgColor = [UIColor colorWithRed:251/255.0f green:251/255.0f blue:251/255.0f alpha:1.0f];
+      
+      // Custom initialization
+      [self changeBG];
+      [self initNavBar];
+    }
+    return self;
+}
+
+- (void)initNavBar
+{
+  //set back button color
+  [[UIBarButtonItem appearanceWhenContainedIn:[UINavigationBar class], nil] setTitleTextAttributes:[NSDictionary dictionaryWithObjectsAndKeys:[UIColor whiteColor], UITextAttributeTextColor,nil] forState:UIControlStateNormal];
+  //set back button arrow color
+  [self.navigationController.navigationBar setTintColor:[UIColor whiteColor]];
+  
+}
+
+- (void)changeBG
+{
+  self.view.backgroundColor = self.bgColor;
+}
+
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
